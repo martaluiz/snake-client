374af089ddb7982a7240962b36e167a5043a01f8
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-12 22:53:20 +0000

    Move Commands

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const connect = function() {
+
   const conn = net.createConnection({ 
     host: '192.168.88.177',
     port: 50541
@@ -11,13 +11,25 @@ const connect = function() {
     console.log('Server says', data);
   });
 
-  conn.on('connect', () => {
+  conn.on('connect', (data) => {
     console.log('Successfully connected to game server" ');
     conn.write("Name: ML");
-  });
-
+    // setInterval(() => {
+    //   conn.write('Move: up')
+    // }, 200);
+    // setInterval(() => {
+    //   conn.write('Move: down')
+    // }, 200);
+    // setInterval(() => {
+    //   conn.write('Move: left')
+    // }, 200);
+    // setInterval(() => {
+    //   conn.write('Move: right')
+    // }, 200);
+     });
   
+ 
   return conn;
-}
+
 
 module.exports = { connect };
