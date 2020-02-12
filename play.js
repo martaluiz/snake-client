1a0970b98052a9328180ed7fe02ca5e7c4f74d0f
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-12 23:17:55 +0000

    Handling CTRL + C User Input

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
-// play.js
 const { connect } = require('./client');
+const stdin = process.stdin;
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data' , handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(data) {
+  if (data === "\u0003") { //ctrl + c	  if (key === "\u0003") { //ctrl + c
+    process.exit()
+}
+};
+
 console.log(' =) =) Connecting ...');
-connect();
+setupInput();
+handleUserInput();
+
